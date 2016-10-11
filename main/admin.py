c3194d077c8b6c12873a1c881f9503067f8f4b72
poleha <pass1191>
2016-10-11 21:05:21 +0300

    Admin added

--- main/admin.py
+++ main/admin.py
@@ -5,3 +5,21 @@ from . import models
 @admin.register(models.VendorProfile)
 class VendorProfileAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.ProductInCart)
+class ProductInCartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SocialAccount)
+class SocialAccountAdmin(admin.ModelAdmin):
+    pass
